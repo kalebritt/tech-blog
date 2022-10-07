4d0a6fcf597de31db04ed0942736b004df64c578
kalebritt <68469962+kalebritt@users.noreply.github.com>
2022-10-07 16:08:32 -0600

    working on server; not connected yet

--- server.js
+++ server.js
@@ -24,3 +24,20 @@ const sess = {
     db: sequelize,
   }),
 };
+
+app.use(session(sess));
+
+const hbs = exphbs.create({ helpers });
+app.engine("handlebars", hbs.engine);
+app.set("view engine", "handlebars");
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, "public")));
+
+app.use(require("./controllers"));
+
+app.listen(PORT, () => {
+  console.log("Copy loud and clear on ${PORT}!");
+  sequelize.sync({ force: false });
+});
