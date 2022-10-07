b3b7b3541f020d20d638739182a03c26d8c3eb53
kalebritt <68469962+kalebritt@users.noreply.github.com>
2022-10-07 12:56:51 -0600

    added cookies

--- server.js
+++ server.js
@@ -9,3 +9,18 @@ const PORT = process.env.PORT || 3001;
 
 const sequelize = require("./config/config");
 const SequelizeStore = require("connect-session-sequelize")(session.Store);
+
+const sess = {
+  secret: " Super secret secret",
+  cookie: {
+    maxAge: 30000,
+    httpOnly: true,
+    secure: false,
+    sameSite: "strict",
+  },
+  resave: false,
+  saveUninitiated: true,
+  store: new SequelizeStore({
+    db: sequelize,
+  }),
+};
