96a9ea66bb9d790e249ff9f7f931b3dc0e09525d
kalebritt <68469962+kalebritt@users.noreply.github.com>
2022-10-08 17:05:36 -0600

    finished 'belongsTo' code block in index.js

--- models/index.js
+++ models/index.js
@@ -1,3 +1,18 @@
 const User = require("./User");
 const Post = require("./Post");
-const Comment = requre("./Comment.js");
+const Comment = requre("./Comment");
+
+Post.belongsTo(User, {
+  foreignKey: "userId",
+  onDelete: "CASCADE",
+});
+
+Post.hasMany(Comment, {
+  foreignKey: "postID",
+  onDelete: "CASCADE",
+});
+
+Comment.belongsTo(User,{
+    foreignKey: 'userId',
+    onDelete: 'CASCADE'
+});
