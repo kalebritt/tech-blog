9ced9d9698a2c643468956cb9324c5b17c8387e4
kalebritt <68469962+kalebritt@users.noreply.github.com>
2022-10-08 10:44:38 -0600

    completed dashboard_routes.js

--- controllers/dashboard_routes.js
+++ controllers/dashboard_routes.js
@@ -19,3 +19,23 @@ router.get("/", withAuth, async (req, res) => {
     res.redirect("login");
   }
 });
+
+router.get("/edit/:id", withAuth, async (req, res) => {
+  try {
+    const postData = await Post.findByPk(req.params.id);
+
+    if (postData) {
+      const post = postData.get({ plain: true });
+      res.render("edit-post", {
+        layout: "dashboard",
+        post,
+      });
+    } else {
+      res.status(404).end();
+    }
+  } catch (err) {
+    res.redirect("login");
+  }
+});
+
+module.exports = router;
