3bde7b8b2ed7dbcd1fcb97acfc5d11b47692a1f4
kalebritt <68469962+kalebritt@users.noreply.github.com>
2022-10-08 16:26:26 -0600

    added consts and some comments

---
+++ models/User.js
@@ -0,0 +1,7 @@
+const { Model, DataTypes } = require('sequelize');
+const bcrypt = require('bcrypt');
+const sequelize = require('../config/config')
+
+//create user model
+
+//setup method (per user)
