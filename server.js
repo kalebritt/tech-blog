8b9a6ed053a3e0ca885b9fa8941552e2db746464
kalebritt <68469962+kalebritt@users.noreply.github.com>
2022-10-08 07:58:58 -0600

    minor adjustments in server.js

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ const sess = {
   }),
 };
 
-// app.use(session(sess));
+app.use(session(sess));
 
 const hbs = exphbs.create({ helpers });
 app.engine("handlebars", hbs.engine);
@@ -35,7 +35,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
 
-app.use(require("./controllers"));
+app.use(require("./controllers/"));
 
 app.listen(PORT, () => {
   console.log("Copy loud and clear on ${PORT}!");
