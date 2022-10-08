f8614af1ec2d2d965cf1de720278c64d0c25fa1d
kalebritt <68469962+kalebritt@users.noreply.github.com>
2022-10-08 07:54:20 -0600

    added api folder in controllers and all routes

--- config/config.js
+++ config/config.js
@@ -7,7 +7,7 @@ const sequelize = process.env.JAWSDB_URL
   : new Sequelize(
       process.env.DB_NAME,
       process.env.DB_USER,
-      profess.env.DB_PASSWORD,
+      process.env.DB_PASSWORD,
       {
         hose: "localhost",
         dialect: "mysql",
