0455c96836c59354300af8973ef0ab97812aec4a
kalebritt <68469962+kalebritt@users.noreply.github.com>
2022-10-08 16:28:51 -0600

    working on create user model in User.js

--- models/User.js
+++ models/User.js
@@ -1,7 +1,11 @@
-const { Model, DataTypes } = require('sequelize');
-const bcrypt = require('bcrypt');
-const sequelize = require('../config/config')
+const { Model, DataTypes } = require("sequelize");
+const bcrypt = require("bcrypt");
+const sequelize = require("../config/config");
 
 //create user model
+class User extends Model {
+  checkPassword(loginPw) {
+    return bcrypt.compareSync(loginPw, this.password);
+  }
+}
 
-//setup method (per user)
