56a0278eb5ecd72815cd887dd08c0e948bded220
kalebritt <68469962+kalebritt@users.noreply.github.com>
2022-10-08 16:39:34 -0600

    working on hooks in User.js

--- models/User.js
+++ models/User.js
@@ -9,3 +9,32 @@ class User extends Model {
   }
 }
 
+User.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    username: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [4],
+      },
+    },
+  },
+  {
+    hooks: {
+      beforeCreate: async (newUserData) => {
+        newUserData.password = await bcrypt.hash(newUserData.password, 10);
+        return newUserData;
+      },
+    },
+  }
+);
