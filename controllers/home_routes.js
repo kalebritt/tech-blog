d7663c011b9ec122f54293129427ad998731db6e
kalebritt <68469962+kalebritt@users.noreply.github.com>
2022-10-08 11:52:30 -0600

    added 'get single' post code block

--- controllers/home_routes.js
+++ controllers/home_routes.js
@@ -21,11 +21,24 @@ router.get("/", async (req, res) => {
 
 //'get single' post
 router.get("/post/:id", async (req, res) => {
-  include: [
-    User,
-    {
-      model: Comment,
-      include: [User],
-    },
-  ];
+  try {
+    const postData = await Post.findByPk(req.params.id, {
+      include: [
+        User,
+        {
+          model: Comment,
+          include: [User],
+        },
+      ],
+    });
+    if (postData) {
+      const post = postData.get({ post });
+
+      res.render("single-post", { post });
+    } else {
+      res.status(404).end();
+    }
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
