2a096fe70007b3257c7148a1fe8645a96e2f2792
kalebritt <68469962+kalebritt@users.noreply.github.com>
2022-10-08 12:22:15 -0600

    completed index.js in controllers

---
+++ controllers/index.js
@@ -0,0 +1,11 @@
+const router = require("express").Router();
+
+const apiRoutes = require("./api/");
+const homeRoutes = require("./home_routes");
+const dashboardRoutes = require("./dashboard_routes");
+
+router.use("/", homeRoutes);
+router.use("/dashboard", dashboardRoutes);
+router.use("/api", apiRoutes);
+
+module.exports = router;
