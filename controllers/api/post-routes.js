8d1f02b3d3a5cdea3b5eb452cd4e79f4498ffa61
kalebritt <68469962+kalebritt@users.noreply.github.com>
2022-10-09 08:35:41 -0600

    added post route and comments in post-routes.js

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -1,6 +1,18 @@
 //put up your consts here
-const router = require('express').Router();
-const { Post } = require('../../models/');
-const withAuth = require('../../utils/auth');
+const router = require("express").Router();
+const { Post } = require("../../models/");
+const withAuth = require("../../utils/auth");
 
 //post route
+router.post("/", withAuth, async (req, res) => {
+  const body = req.body;
+
+  try {
+    const newPost = await Post.creat({ ...body, userId: req.session.userId });
+    res.json(newPost);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+//put route
