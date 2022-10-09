7cb898fb5bdb64a721ebefe9eabe327a5fa9df74
kalebritt <68469962+kalebritt@users.noreply.github.com>
2022-10-09 08:08:31 -0600

    completed index.js

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,8 +1,13 @@
 //routes!
-const router = require('express').Router();
+const router = require("express").Router();
 
-const userRoutes = require('./user_routes');
-const postRoutes = require('./post-routes')
-const commentRoutes = require('./comment-routes')
+const userRoutes = require("./user_routes");
+const postRoutes = require("./post-routes");
+const commentRoutes = require("./comment-routes");
 
 //router.use here
+router.use("/user", userRoutes);
+router.use("/post", postRoutes);
+router.use("/comment", commentRoutes);
+
+module.exports = router;
