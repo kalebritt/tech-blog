4aad89a67b7af7d84a7d4eaabb8027c13a9c5a00
kalebritt <68469962+kalebritt@users.noreply.github.com>
2022-10-09 08:07:14 -0600

    added all consts and some comments in index.js

---
+++ controllers/api/index.js
@@ -0,0 +1,8 @@
+//routes!
+const router = require('express').Router();
+
+const userRoutes = require('./user_routes');
+const postRoutes = require('./post-routes')
+const commentRoutes = require('./comment-routes')
+
+//router.use here
