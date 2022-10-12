9e46d952440a167382fbc85ee266356bb7156278
kalebritt <68469962+kalebritt@users.noreply.github.com>
2022-10-11 18:00:20 -0600

    completed post route

--- controllers/api/user_routes.js
+++ controllers/api/user_routes.js
@@ -1,12 +1,21 @@
-const router = require('express').Router();
-const { User } = require('../../models');
+const router = require("express").Router();
+const { User } = require("../../models");
 
-router.post('/', async (req, res) => {
-    try {
-        const newUser = await User.create({
-            username: req.body.username,
-            password: req.body.password,
-        });
-        
-    }
-})
+//post route
+router.post("/", async (req, res) => {
+  try {
+    const newUser = await User.create({
+      username: req.body.username,
+      password: req.body.password,
+    });
+    req.session.save(() => {
+      req.session.userId = newUser.id;
+      req.sessionID.username = newUser.username;
+      req.session.loggedIn = true;
+
+      res.json(newUser);
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
