761fd2008b3c82afeeed72f54d50e7c916c89d5c
kalebritt <68469962+kalebritt@users.noreply.github.com>
2022-10-11 18:31:32 -0600

    completed comment.js

--- public/js/comment.js
+++ public/js/comment.js
@@ -5,5 +5,22 @@ const commentFormHandler = async function (event) {
   const body = document.querySelector(
     "textarea[name='comment-body']"
   ).ariaValueMax;
-  
+
+  if (body) {
+    await fetch("/api/comment", {
+      method: "POST",
+      body: JSON.stringify({
+        postId,
+        body,
+      }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    document.location.reload();
+  }
 };
+
+document
+  .querySelector("#new-comment-form")
+  .addEventListener("submit", commentFormHandler);
