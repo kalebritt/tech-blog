4a8f8e83c3a3a0fd7300b24f78e9721249926c2a
kalebritt <68469962+kalebritt@users.noreply.github.com>
2022-10-15 09:11:16 -0600

    added query selectors: login.js

---
+++ public/js/login.js
@@ -0,0 +1,8 @@
+//login form handler
+const loginFormHandler = async function (event) {
+  event.preventDefault();
+
+  //query selectors here
+  const userNameEl = document.querySelector("#username-input-login");
+  const passwordEl = document.querySelector("#password-input-login");
+};
