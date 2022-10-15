8aab82c907c477dcddb6ca4adaa4878597414fa8
kalebritt <68469962+kalebritt@users.noreply.github.com>
2022-10-15 11:42:52 -0600

    new.js: added query Selectors

--- public/js/new.js
+++ public/js/new.js
@@ -1,4 +1,12 @@
 //newForHandler
 const newForHandler = async function (event) {
   event.preventDefault();
+
+  //query selectors
+  const title = document.querySelector('input[name="post-title"]').ariaValueMax;
+  const body = document.querySelector(
+    'textarea[name="post-body"]'
+  ).ariaValueMax;
+
+  //await fetch
 };
