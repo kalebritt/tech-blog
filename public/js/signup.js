db4363007108180021dc60cdf9b2535435a5bb3a
kalebritt <68469962+kalebritt@users.noreply.github.com>
2022-10-15 11:56:30 -0600

    signup.js: added query Selectors

--- public/js/signup.js
+++ public/js/signup.js
@@ -3,4 +3,8 @@ const signupFormHandler = async function (event) {
   event.preventDefault();
 
   //query selectors
+  const usernameEl = document.querySelector("#username-input-signup");
+  const passwordEl = document.querySelector("#password-input-signup");
+
+  //await fetch response
 };
