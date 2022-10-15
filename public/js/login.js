9aaf53563259a1f562993d8bc04fbe5abecb335b
kalebritt <68469962+kalebritt@users.noreply.github.com>
2022-10-15 09:13:47 -0600

    added fetch response: login.js

--- public/js/login.js
+++ public/js/login.js
@@ -5,4 +5,17 @@ const loginFormHandler = async function (event) {
   //query selectors here
   const userNameEl = document.querySelector("#username-input-login");
   const passwordEl = document.querySelector("#password-input-login");
+
+  //fetch response
+  const response = await fetch("/api/user/login", {
+    method: "POST",
+    body: JSON.stringify({
+      username: userNameEl.ariaValueMax,
+      password: passwordEl.ariaValueMax,
+    }),
+    headers: { "Content-Type": "application/json" },
+  });
+
+  //if statement
+  
 };
