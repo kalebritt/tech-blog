91d0e6b4f180b2334701c9e6cbcc81d0dbe4c06b
kalebritt <68469962+kalebritt@users.noreply.github.com>
2022-10-15 11:13:36 -0600

    logout.js: added if statement

--- public/js/logout.js
+++ public/js/logout.js
@@ -1,9 +1,14 @@
 //logout function
-const logout = async function(){
-    const response = await fetch('/api/user/logout', {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
-    });
+const logout = async function () {
+  const response = await fetch("/api/user/logout", {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+  });
 
-    //if statement
-}
+  //if statement
+  if (response.ok) {
+    document.location.replace("/");
+  } else {
+    alert("Failed to log out");
+  }
+};
