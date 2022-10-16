7d1a8f4302500be34ce438ea702eddadb8f65fc8
kalebritt <68469962+kalebritt@users.noreply.github.com>
2022-10-16 07:57:43 -0600

    signup.js: added await fetch

--- public/js/signup.js
+++ public/js/signup.js
@@ -7,4 +7,14 @@ const signupFormHandler = async function (event) {
   const passwordEl = document.querySelector("#password-input-signup");
 
   //await fetch response
+  const response = await fetch("/api/user", {
+    method: "POST",
+    body: JSON.stringify({
+      username: usernameEl.value,
+      password: passwordEl.value,
+    }),
+    headers: { "Content-Type": "application/json" },
+  });
+  //if statement
+  if (respo)
 };
