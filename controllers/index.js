a7eee038c66f32177315c95a0dc48461ae0536e5
kalebritt <68469962+kalebritt@users.noreply.github.com>
2022-10-18 13:53:56 -0600

    changed _ to -

--- controllers/index.js
+++ controllers/index.js
@@ -1,8 +1,8 @@
 const router = require("express").Router();
 
 const apiRoutes = require("./api/");
-const homeRoutes = require("./home_routes.js");
-const dashboardRoutes = require("./dashboard_routes.js");
+const homeRoutes = require("./home-routes.js");
+const dashboardRoutes = require("./dashboard-routes.js");
 
 router.use("/", homeRoutes);
 router.use("/dashboard", dashboardRoutes);
