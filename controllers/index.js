e348291a21295b9028a247455a465e84cca96012
kalebritt <68469962+kalebritt@users.noreply.github.com>
2022-10-18 13:47:50 -0600

    index.js: added .js to const routes

--- controllers/index.js
+++ controllers/index.js
@@ -1,8 +1,8 @@
 const router = require("express").Router();
 
 const apiRoutes = require("./api/");
-const homeRoutes = require("./home_routes");
-const dashboardRoutes = require("./dashboard_routes");
+const homeRoutes = require("./home_routes.js");
+const dashboardRoutes = require("./dashboard_routes.js");
 
 router.use("/", homeRoutes);
 router.use("/dashboard", dashboardRoutes);
