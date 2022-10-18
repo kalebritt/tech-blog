22e62a967bd93d55d8f7042fcab1af3d0cfbc369
kalebritt <68469962+kalebritt@users.noreply.github.com>
2022-10-18 14:17:58 -0600

    changed ariaValueMax in consts to value

--- public/js/comment.js
+++ public/js/comment.js
@@ -1,10 +1,10 @@
 const commentFormHandler = async function (event) {
   event.preventDefault();
 
-  const postId = document.querySelector("input[name='post-id']").ariaValueMax;
+  const postId = document.querySelector("input[name='post-id']").value;
   const body = document.querySelector(
     "textarea[name='comment-body']"
-  ).ariaValueMax;
+  ).value;
 
   if (body) {
     await fetch("/api/comment", {
